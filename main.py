8a74ff823565bfe521d28b7eb4d5a3e089f0f6e8
Giovanni La Cagnina <giova.lacagnina22@gmail.com>
2023-01-18 14:41:57 +0100

    update

--- main.py
+++ main.py
@@ -35,18 +35,14 @@ def retrieve_data(assets, Mconfig, dates):
             name=f"{name}USDT",
             dates=dates,
             n_job=n_job,
-            type_="klines",
+            type_=Mconfig["type_data"],
             folder=raw_folder,
             frequency=Mconfig["frequency"],
             return_path=True,
         )
         print("\n")
 
-        if Mconfig["drive"]:
-            id_folder = CreateFolder(
-                folder_id=Mconfig["idx_klines"], folderName=name, return_id=True
-            )
-            UploadFileListData(files=paths, folder_id=id_folder)
+        
 
 
 def create_unique_dataset(Mconfig, assets, dates):
@@ -130,6 +126,7 @@ def apply_strategy_pair(pair, coin_df, Mconfig, config, path_result):
         dpi=80,
     )
     
+    
 def beta_table(pair, coin_df, Mconfig, config, path_result):
         
         beta_table, path_beta = create_beta_table(
@@ -148,27 +145,54 @@ def beta_table(pair, coin_df, Mconfig, config, path_result):
             UploadFile(file=path_beta, folder_id=id_pair)
     
 
+    
+def retrieve_clean_dataset(Mconfig):
+    #retrieve data from google drive 
+    idx = Mconfig[f"idx_{Mconfig['type_data']}"]
+    files =  FilesAvailableDrive(idx)
+    RetrieveFile(files, idx)
+    #create the clean folder
+    clean_folder = f"{Mconfig['output_folder']}/cleandata"
+    os.makedirs(clean_folder, exist_ok=True)
+    #move the file 
+    shutil.move(files[0]["title"], clean_folder)
+    #load the file 
+    df = loadCleanDataFrame(f"{clean_folder}/{files[0]['title']}")
+    return df 
+                
+    
+    
+    
 
 def main():
     matplotlib.use('agg')
     # load configuration run main
     Mconfig = open_config(path="config/Mconfig.json")
     config = open_config(path="config/config.json")
-
+    
     # load assets to use
     assets = load_assets(Mconfig)
     # creating list of pairs
     pairs = create_pairs(assets)
     # create list of dates
     dates = create_listdates(Mconfig["dates_interval"])
+    
     if Mconfig["toy"]:
         # if toy example run consider only three months
         if len(dates) >= 3:
             dates = dates[:3]
+            
+            
     # retrieve the data
-    retrieve_data(assets, Mconfig, dates)
-    # create clean dataframe
-    coin_df = create_unique_dataset(Mconfig, assets, dates)
+    if Mconfig["download_clean_data"] == False:
+        print("Downloading raw data")
+        retrieve_data(assets, Mconfig, dates)
+        # create clean dataframe
+        coin_df = create_unique_dataset(Mconfig, assets, dates)
+    else:
+        print("Loading clean data")
+        coin_df = retrieve_clean_dataset(Mconfig)
+    
     # create result folder
     path_result = create_result_folder(Mconfig["output_folder"])
     # calculate the beta table
