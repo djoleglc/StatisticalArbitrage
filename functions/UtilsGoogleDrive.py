b997c57be807c607df63ee60af96b5a5dd704826
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2023-02-02 22:19:43 +0100

    Update UtilsGoogleDrive.py

--- functions/UtilsGoogleDrive.py
+++ functions/UtilsGoogleDrive.py
@@ -4,6 +4,8 @@ import os
 import shutil
 import joblib
 
+
+
 def UploadFile(file, folder_id):
     """
     Upload a file to a specified Google Drive folder.
@@ -25,8 +27,22 @@ def UploadFile(file, folder_id):
     gfile.SetContentFile(file)
     gfile.Upload()
 
-
+    
+    
+    
 def UploadFileListData(files, folder_id):
+    """
+    Function to upload a list of files to a folder in Google Drive.
+    
+    Input:
+      - files: list
+            List of files to be uploaded.
+      - folder_id: str
+            ID of the Google Drive folder where the files will be uploaded to.
+            
+    Output:
+      - None
+    """
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
     for file in files:
@@ -36,10 +52,26 @@ def UploadFileListData(files, folder_id):
         gfile["title"] = name_file
         gfile.SetContentFile(file)
         gfile.Upload()  # Upload the file
-
-
+      
+    
+    
+        
 def CreateFolder(folder_id, folderName, return_id=False):
-
+    """
+    Function to create a folder in Google Drive.
+    
+    Input:
+      - folder_id: str
+            ID of the parent folder in Google Drive where the new folder will be created in.
+      - folderName: str
+            Name of the folder to be created.
+      - return_id: bool
+            Flag indicating whether the ID of the created folder should be returned or not.
+            
+    Output:
+      - folder_id: str (if return_id is True)
+            ID of the created folder in Google Drive.
+    """
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
     file_list = FilesAvailableDrive(folder_id)
@@ -59,9 +91,22 @@ def CreateFolder(folder_id, folderName, return_id=False):
         id_ = [j["id"] for j in file_list if j["title"] == folderName]
         if len(id_) > 0:
             return id_[0]
-
-
+           
+            
+            
+            
 def FilesAvailableDrive(folder_id):
+    """
+    Function to retrieve a list of files in a Google Drive folder.
+    
+    Input:
+      - folder_id: str
+            ID of the Google Drive folder to retrieve the list of files from.
+            
+    Output:
+      - file_list: list
+            List of files in the Google Drive folder.
+    """
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
     file_list = drive.ListFile(
@@ -70,7 +115,22 @@ def FilesAvailableDrive(folder_id):
     return file_list
 
 
+
+
+
 def RetrieveFile(file_list, folder_id):
+    """
+    Function to retrieve files from a Google Drive folder.
+    
+    Input:
+      - file_list: list
+            List of files in the Google Drive folder.
+      - folder_id: str
+            ID of the Google Drive folder to retrieve the files from.
+            
+    Output:
+      None
+    """
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
 
@@ -81,9 +141,26 @@ def RetrieveFile(file_list, folder_id):
             )
         )
         file.GetContentFile(file["title"])
-
-
+        
+        
+        
+    
 def CreatePairFolder(folder_id, pair):
+    """
+    Function to create a folder in Google Drive with the title derived from a pair of assets.
+    
+    Input:
+      - folder_id: str
+            ID of the parent Google Drive folder.
+      - pair: list
+            Pair of assets to derive the title for the folder.
+            
+    Output:
+      - title: str
+            Title of the folder.
+      - id_: str
+            ID of the folder.
+    """
     asset_1 = pair[0]
     asset_2 = pair[1]
     title = f"{asset_1}_{asset_2}"
@@ -98,7 +175,23 @@ def CreatePairFolder(folder_id, pair):
     return title, id_
 
 
+
+
 def RetrieveFolder(files, name_folder, output_folder="H:"):
+    """
+    Function to retrieve a Google Drive folder and move it to a local directory.
+    
+    Input:
+      - files: list
+            List of files in Google Drive.
+      - name_folder: str
+            Name of the Google Drive folder to be retrieved.
+      - output_folder: str (optional)
+            Path to the local directory where the retrieved folder should be moved to. Default is "H:".
+            
+    Output:
+      None
+    """
     current_dir = os.getcwd()
     id_ = [j["id"] for j in files if j["title"] == name_folder][0]
     ls = FilesAvailableDrive(id_)
@@ -118,10 +211,32 @@ def RetrieveFolder(files, name_folder, output_folder="H:"):
     if output_folder is not None:
         shutil.move(name_folder, output_folder)
 
-
+        
+        
+        
 def SaveUploadResultStrategy(
     result, asset_name_1, asset_name_2, idx, drive=True, output_folder="H:"
 ):
+    """
+    Function to save the results of a calculation and upload the saved file to Google Drive.
+    
+    Input:
+      - result: object
+            The result of the calculation to be saved.
+      - asset_name_1: str
+            First name of the asset.
+      - asset_name_2: str
+            Second name of the asset.
+      - idx: str
+            ID of the Google Drive folder where the saved file should be uploaded to.
+      - drive: bool (optional)
+            Boolean value indicating whether to upload the saved file to Google Drive or not. Default is True.
+      - output_folder: str (optional)
+            Path to the local directory where the saved file should be stored. Default is "H:".
+            
+    Output:
+      None
+    """
     folder_name = f"{asset_name_1}_{asset_name_2}"
     path_result = os.path.join(output_folder, f"{folder_name}/{folder_name}.joblib")
     joblib.dump(result, path_result)
