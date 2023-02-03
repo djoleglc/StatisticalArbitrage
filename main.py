c007997da344bd4d9e664ccf2b7061a917a7e7c6
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2023-02-03 20:48:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import joblib
 import warnings
 from multiprocessing.dummy import Pool
 import os
+import itertools
 from itertools import combinations
 import pandas as pd
 from functions.UtilsRetrieveData import downloadList, create_listdates
@@ -12,20 +13,37 @@ from functions.Strategy import *
 import matplotlib
 
 def open_config(path):
+    """
+    Loads a JSON configuration file from the given path and returns its contents as a Python object.
+    
+    Input:
+      - path: str
+        The path to the JSON configuration file to be opened.
+    
+    Output: 
+      - to_return: object
+        The contents of the JSON configuration file as a Python object.
+    """
     with open(path) as json_file:
         to_return = json.load(json_file)
     return to_return
 
 
-def load_assets(Mconfig):
-    assets = joblib.load(Mconfig["list_assets_path"])
-    if Mconfig["toy"]:
-        return assets[0:3]
-    else:
-        return assets
-
-
 def retrieve_data(assets, Mconfig, dates):
+    """
+    Downloads data for a list of assets for a specified set of dates, and saves the data in a specified folder.
+    
+    Input:
+      - assets: list or array-like
+        A list or array-like object containing the assets for which data should be retrieved.
+      - Mconfig: dict
+        A dictionary that contains the configuration parameters, including the number of parallel jobs to run, the type of data to retrieve, and the frequency of data to retrieve.
+      - dates: list or array-like
+        A list or array-like object containing the dates for which data should be retrieved.
+    
+    Output: 
+      None
+    """
     n_job = Mconfig["n_job"]
     raw_folder = f"{Mconfig['output_folder']}/rawdata"
     os.makedirs(raw_folder, exist_ok=True)
@@ -46,6 +64,19 @@ def retrieve_data(assets, Mconfig, dates):
 
 
 def create_unique_dataset(Mconfig, assets, dates):
+    """
+    Function to create a unique dataset of all assets and to save it.
+    Input:
+      - Mconfig: dict
+            contains all the configs of the experiment
+      - assets: list
+            list of the assets to consider
+      - dates: list
+            list of the dates to consider
+    Output: 
+      - df : pd.DataFrame
+            dataframe containing all the data of the assets
+    """
     input_folder = f"{Mconfig['output_folder']}/rawdata"
     clean_folder = f"{Mconfig['output_folder']}/cleandata"
     os.makedirs(clean_folder, exist_ok=True)
@@ -61,25 +92,67 @@ def create_unique_dataset(Mconfig, assets, dates):
         to_save=to_save,
         output_name=f"AllTickers_klines_{Mconfig['frequency']}",
         output_folder=clean_folder,
-        type_="klines",
+        type_=Mconfig["type_data"],
         frequency=Mconfig["frequency"],
     )
     return df
 
 
 def create_result_folder(output_folder):
+    """
+    Function to create a folder to store the results.
+    
+    Input:
+      - output_folder: str
+            Path to the main output folder.
+            
+    Output:
+      - path: str
+            Path to the folder for storing results.
+    """
     path = f"{output_folder}/results"
     os.makedirs(path, exist_ok=True)
     return path
 
 
-def create_pairs(assets_list):
+def create_pairs(Mconfig):
+    """
+    Function to load and return a list of pairs from the stored file.
+    
+    Input:
+      - assets_list: list
+            List of assets.
+            
+    Output:
+      - pairs: list
+            List of pairs.
+    """
     pairs = joblib.load("config/all_pairs.joblib")
+    if Mconfig["toy"]:
+        pairs = pairs[0:5]
     return pairs
 
 
 def apply_strategy(coin_df, pair, Mconfig, config, path_result):
-
+    """
+    Function to apply a trading strategy and save the results.
+    
+    Input:
+      - coin_df: pd.DataFrame
+            DataFrame containing the data for a particular asset.
+      - pair: tuple
+            Tuple of two asset names as a pair.
+      - Mconfig: dict
+            Dictionary containing the main configuration parameters.
+      - config: dict
+            Dictionary containing the configuration parameters for the strategy.
+      - path_result: str
+            Path to the folder for storing results.
+            
+    Output:
+      - result: dict
+            Dictionary containing the results of the strategy.
+    """
     result = getCombRet(
         coin_df=coin_df,
         asset_name_1=pair[0],
@@ -104,6 +177,24 @@ def apply_strategy(coin_df, pair, Mconfig, config, path_result):
 
 
 def apply_strategy_pair(pair, coin_df, Mconfig, config, path_result):
+    """
+    Function to apply the strategy and create the plots for a given pair.
+
+    Input:
+      - pair: list
+            A list of two elements representing a single trading pair.
+      - coin_df: pandas.DataFrame
+            DataFrame containing the asset data.
+      - Mconfig: dict
+            Dictionary containing the global configurations.
+      - config: dict
+            Dictionary containing the configurations specific to the strategy.
+      - path_result: str
+            Path to the folder where results are stored.
+            
+    Output:
+      - None
+    """
     # applying the strategy
     result = apply_strategy(
         coin_df=coin_df,
@@ -125,11 +216,31 @@ def apply_strategy_pair(pair, coin_df, Mconfig, config, path_result):
         idx=None,
         dpi=80,
     )
-    
+
     
 def beta_table(pair, coin_df, Mconfig, config, path_result):
-        
-        beta_table, path_beta = create_beta_table(
+    """
+    Function to create a beta table and save it to the specified path.
+    
+    Input:
+      - pair: tuple
+            Tuple of two assets to form a pair.
+      - coin_df: pandas dataframe
+            Dataframe containing price information for the assets.
+      - Mconfig: dict
+            Dictionary of main configuration parameters.
+      - config: dict
+            Dictionary of configuration parameters specific to this function.
+      - path_result: str
+            Path to store the results of the function.
+            
+    Output:
+      - beta_table: pandas dataframe
+            Dataframe containing beta information for the pair.
+      - path_beta: str
+            Path to the saved beta table.
+    """
+    beta_table, path_beta = create_beta_table(
         coin_df=coin_df,
         asset_name_1=pair[0],
         asset_name_2=pair[1],
@@ -139,26 +250,41 @@ def beta_table(pair, coin_df, Mconfig, config, path_result):
         n_job=Mconfig["n_job"],
         output_folder=path_result,
         stat_test=config["stat_test"],
-        )
+    )
 
-        if Mconfig["drive"]:
-            UploadFile(file=path_beta, folder_id=id_pair)
-    
+    return beta_table, path_beta
 
     
-def retrieve_clean_dataset(Mconfig):
-    #retrieve data from google drive 
-    idx = Mconfig[f"idx_{Mconfig['type_data']}"]
-    files =  FilesAvailableDrive(idx)
+def retrieve_clean_dataset(Mconfig, config):
+    """
+    Function to retrieve and return the clean dataset from a google drive file.
+    
+    Input:
+      - Mconfig: dict
+            Dictionary of configuration parameters.
+      - config: dict
+            Dictionary of configuration parameters.
+            
+    Output:
+      - df: pandas DataFrame
+            Clean dataset.
+    """
+    # retrieve data from google drive
+    idx = config[f"idx_{Mconfig['type_data']}"]
+    files = FilesAvailableDrive(idx)
     RetrieveFile(files, idx)
-    #create the clean folder
+    # create the clean folder
     clean_folder = f"{Mconfig['output_folder']}/cleandata"
     os.makedirs(clean_folder, exist_ok=True)
-    #move the file 
-    shutil.move(files[0]["title"], clean_folder)
-    #load the file 
+    # move the file
+    if files[0]["title"] not in os.listdir(clean_folder):
+        shutil.move(files[0]["title"], clean_folder)
+    else:
+        os.remove(f"{clean_folder}/{files[0]['title']}")
+        shutil.move(files[0]["title"], clean_folder)
+    # load the file
     df = loadCleanDataFrame(f"{clean_folder}/{files[0]['title']}")
-    return df 
+    return df
                 
     
     
@@ -170,17 +296,26 @@ def main():
     Mconfig = open_config(path="config/Mconfig.json")
     config = open_config(path="config/config.json")
     
-    # load assets to use
-    assets = load_assets(Mconfig)
+    if Mconfig["output_folder"] is None:
+        Mconfig["output_folder"] = os.getcwd()
+    
+    
+    
     # creating list of pairs
-    pairs = create_pairs(assets)
+    pairs = create_pairs(Mconfig)
+
+    # assets to use
+    assets = list(set(itertools.chain(*pairs)))
+ 
     # create list of dates
-    dates = create_listdates(Mconfig["dates_interval"])
+    dates = create_listdates(config["dates_interval"])
     
     if Mconfig["toy"]:
         # if toy example run consider only three months
         if len(dates) >= 3:
+            dates_ = dates
             dates = dates[:3]
+            next_date = dates_[3]
             
             
     # retrieve the data
@@ -191,7 +326,10 @@ def main():
         coin_df = create_unique_dataset(Mconfig, assets, dates)
     else:
         print("Loading clean data")
-        coin_df = retrieve_clean_dataset(Mconfig)
+        coin_df = retrieve_clean_dataset(Mconfig, config)
+        if Mconfig["toy"]:
+            coin_df = coin_df[coin_df.index < next_date]
+            
     
     # create result folder
     path_result = create_result_folder(Mconfig["output_folder"])
